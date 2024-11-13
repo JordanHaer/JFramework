0db920f93a27f553c5a7784a0a980df094c79b62
JordanHaer <jordanhaer@gmail.com>
2024-11-13 23:21:40 +0000

    changes

--- JFramework/Framework.swift
+++ JFramework/Framework.swift
@@ -4,4 +4,5 @@ public enum Framework {
     public static let name = "JFramework"
     public static let version = "0.0.1"
     public static let dependencyName = Library.name
+    public static let dependencyVersion = Library.version
 }
--- JFramework/JFramework.h
+++ JFramework/JFramework.h
@@ -1,7 +1,5 @@
 #import <Foundation/Foundation.h>
 
-//! Project version number for JFramework.
 FOUNDATION_EXPORT double JFrameworkVersionNumber;
 
-//! Project version string for JFramework.
 FOUNDATION_EXPORT const unsigned char JFrameworkVersionString[];
