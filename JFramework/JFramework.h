614b97c99afd2e50a517379298de3222a1b8b2b7
JordanHaer <jordanhaer@gmail.com>
2024-11-13 20:34:21 +0000

    Initial Commit

---
+++ JFramework/JFramework.h
@@ -0,0 +1,18 @@
+//
+//  JFramework.h
+//  JFramework
+//
+//  Created by Jordan Haer on 13/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JFramework.
+FOUNDATION_EXPORT double JFrameworkVersionNumber;
+
+//! Project version string for JFramework.
+FOUNDATION_EXPORT const unsigned char JFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JFramework/PublicHeader.h>
+
+
